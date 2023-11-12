dca2836659d52ab47c7ee0517ae0d1c4829b4130
Steve De Jongh <dejongh.st@gmail.com>
2023-11-12 11:57:32 +0100

    updated tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,17 +3,11 @@ from sdj_remotetools.utils import ssh_url_type
 
 
 def test_ssh_url_type():
-    url = 'joe@some.host.local:/path/to/file john@example.lan:/path/to/otherfile'
-    result = [
-        {
+    url = 'joe@some.host.local:/path/to/file'
+    result = {
             'username': 'joe',
             'remote_host': 'some.host.local',
             'path_to_file': '/path/to/file'
-        },
-        {
-            'username': 'john',
-            'remote_host': 'example.lan',
-            'path_to_file': '/path/to/otherfile'
         }
-    ]
+
     assert ssh_url_type(url) == result
