822e92bc86d8fe2c5c3ae53f3b108684060c8bed
Steve De Jongh <dejongh.st@gmail.com>
2023-11-12 01:15:49 +0100

    Added first test

---
+++ tests/test_utils.py
@@ -0,0 +1,11 @@
+from sdj_remotetools.utils import ssh_url_type
+
+
+def test_ssh_url_type():
+    url = 'joe@some.host.local:/path/to/file'
+    result = {
+        'username': 'joe',
+        'remote_host': 'some.host.local',
+        'path_to_file': '/path/to/file'
+    }
+    assert ssh_url_type(url) == result
