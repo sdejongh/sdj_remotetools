0a03d85f92339cb80227bb2ff831d01bc47b0cb2
Steve De Jongh <dejongh.st@gmail.com>
2023-11-12 11:53:50 +0100

    Added support for multiple remote files

--- sdj_remotetools/main.py
+++ sdj_remotetools/main.py
@@ -8,14 +8,22 @@ def rcat():
         prog='rcat',
         description='Works like \'cat\' but through SSH.',
     )
-    parser.add_argument('url', type=ssh_url_type, help='SSH path to the remote file, ie: joe@remote.host:/path/to/file')
-    parser.add_argument('-p', '--password', type=str, help='User password')
+    parser.add_argument('remote_files', nargs='+', type=ssh_url_type, help='SSH path to the remote files, space seperate, ie: joe@remote.host:/path/to/file')
+    parser.add_argument('-p', '--password', action='store_true', help='Ask user for password')
     parser.add_argument('-e', '--encoding', type=str, default='utf-8', help='Character encoding, ie: utf-8 (default)')
+    parser.add_argument('-t', '--title', action='store_true', help='Display header before each file content')
     args = parser.parse_args()
 
-    ssh_args = args.url
-    ssh_args.update({'password': args.password, 'encoding': args.encoding})
+    outputs = []
 
-    remote_file_content = get_remote_file_content(**ssh_args)
+    for remote_file_args in args.remote_files:
+        ssh_args = remote_file_args
+        ssh_args.update({'password': args.password, 'encoding': args.encoding})
+        if args.title:
+            outputs.append(f'##### BEGIN: {ssh_args["remote_host"]}:{ssh_args["path_to_file"]} #####')
+        outputs.append(get_remote_file_content(**ssh_args))
+        if args.title:
+            outputs.append(f'##### END: {ssh_args["remote_host"]}:{ssh_args["path_to_file"]} #####')
+            outputs.append('\n')
 
-    print(remote_file_content)
+    print('\n'.join(outputs))
--- sdj_remotetools/remote_cat.py
+++ sdj_remotetools/remote_cat.py
@@ -1,16 +1,21 @@
 import paramiko
 import sys
+from getpass import getpass
 
 
-def get_remote_file_content(remote_host: str, username: str, password: str or None, path_to_file: str, encoding: str = 'utf8'):
+def get_remote_file_content(remote_host: str, username: str, password: bool or None, path_to_file: str, encoding: str = 'utf8'):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
-        if password is None:
-            ssh.connect(hostname=remote_host, username=username)
+        if password:
+            try:
+                ssh.connect(hostname=remote_host, username=username)
+            except paramiko.SSHException as e:
+                user_password = getpass(f'({remote_host}) Password: ')
+                ssh.connect(hostname=remote_host, username=username, password=user_password)
         else:
-            ssh.connect(hostname=remote_host, username=username, password=password)
+            ssh.connect(hostname=remote_host, username=username)
 
         ftp = ssh.open_sftp()
         content = ftp.open(path_to_file).read().decode(encoding)
